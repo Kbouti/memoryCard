34674004dc6d80774450f1592e13f884c3556577
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-05 11:07:39 -0700

    Create components folder and rename css file

---
+++ src/components/master.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "../style.css";
+
+function MainPage() {
+  return (
+    <>
+      <h1>Memory Card Project</h1>
+    </>
+  );
+}
+
+
+export default MainPage
