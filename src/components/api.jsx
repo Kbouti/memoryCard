34e4d03cb73e114fb9a27945a3b79ed7c9825689
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-08 16:36:52 -0700

    Render first dog gif

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,48 +1,26 @@
 import { useState, useEffect } from "react";
 import "../style.css";
 
-let imageURL = null;
+function GiphyImg() {
+  const [imageURL, setImageURL] = useState("");
 
-async function CallApi() {
-  fetch("https://random.dog/woof.json", { mode: "cors" }).then(function (
-    response
-  ) {
-    console.log(`response: ${response}`);
-    console.log(`response.json(): ${response.json()}`);
-    return response;
-  });
-}
-async function getResponse(){
-  const response  = await CallApi()
-  console.log(`response from callApi: ${response}`);
-  return response
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://random.dog/woof.json");
+      result.json().then(json => {
+        console.log(`json: ${json}`);
+        let newResult = JSON.stringify(json);
+        console.log(`newResult: ${newResult}`);
+        console.log(json.url)
+        setImageURL(json.url);
+      })
+    };
+    fetchData();
+  }, []);
 
-imageURL = getResponse();
-console.log(`imageURL: ${imageURL}`)
-
-// const parsed = JSON.parse(response);
-// console.log(`parsed: ${parsed}`);
-
-// console.log(`response.json(): ${response.json()}`);
-
-// const responseURL = response.data.original.url;
-// console.log(`responseURL: ${responseURL}`);
-
-//  ************************************************************************************************************************
-
-//  This is coming out as [object Reponse]
-//  Can't seem to figure out how to render the giph in the context of jsx components
-
-// I'm almost kinda wondering now if this is a problem with our api call?
-// Yeah I think it's the object we're getting back, not how we're handling it. We need to go back to a basic non-react sandbox and render an image with this object to test
-
-// DO I need to be using useEffect????
-
-//  ************************************************************************************************************************
-
-function GiphyImg(imageSrc) {
-  return <img src={imageSrc}></img>;
+  return <img src={imageURL}></img>;
 }
 
 export default GiphyImg;
+
+// Random dog photo rendered! 
