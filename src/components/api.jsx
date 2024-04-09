8719c868405879fdc894df5ffe58840cd33ccdd7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-09 11:15:37 -0700

    Call api only until we have 12 valid responses

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -19,66 +19,54 @@ function GiphyImg() {
     const fetchData = async () => {
       let urlList = [];
 
-      for (let i = 0; i < 20; i++) {
+      while (urlList.length < 13) {
+        console.log(`iteration`)
+        console.log(`urlList.length: ${urlList.length}`);
         const result = await fetch("https://random.dog/woof.json");
         result.json().then((json) => {
-
-          console.log(`url result: ${json.url}`);
-          // console.log(`to string? ${json.url.toString()}`);
           const toString = json.url.toString();
-           let last3 = toString.substr(toString.length - 3);
-           console.log(`last3: ${last3}`);
+          let last3 = toString.substr(toString.length - 3);
           last3 = last3.toLowerCase();
-          console.log(`last3: ${last3}`);
-
-          if (last3 == "jpg"){
+          if (last3 == "jpg") {
             urlList.push(json.url);
+            console.log(`Added a photo to the list`);
           }
-
-        }).catch((error) => {
-          console.log(error);
-          alert(error);
-          // This seems to work ok but it's not actually loggin an error
         });
-        setImageURL1(urlList[0]);
-        setImageURL2(urlList[1]);
-        setImageURL3(urlList[2]);
-        setImageURL4(urlList[3]);
-        setImageURL5(urlList[4]);
-        setImageURL6(urlList[5]);
-        setImageURL7(urlList[6]);
-        setImageURL8(urlList[7]);
-        setImageURL9(urlList[8]);
-        setImageURL10(urlList[9]);
-        setImageURL11(urlList[10]);
-        setImageURL12(urlList[11]);
       }
+      setImageURL1(urlList[0]);
+      setImageURL2(urlList[1]);
+      setImageURL3(urlList[2]);
+      setImageURL4(urlList[3]);
+      setImageURL5(urlList[4]);
+      setImageURL6(urlList[5]);
+      setImageURL7(urlList[6]);
+      setImageURL8(urlList[7]);
+      setImageURL9(urlList[8]);
+      setImageURL10(urlList[9]);
+      setImageURL11(urlList[10]);
+      setImageURL12(urlList[11]);
     };
+
     fetchData();
   }, []);
 
   return (
-
-      <div className="cardContainer">
-        <img className="dogPic" src={imageURL1} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL2} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL3} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL4} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL5} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL6} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL7} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL8} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL9} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL10} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL11} alt="Random dog picture"></img>
-        <img className="dogPic" src={imageURL12} alt="Random dog picture"></img>
-      </div>
-
+    <div className="cardContainer">
+      <img className="dogPic" src={imageURL1} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL2} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL3} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL4} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL5} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL6} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL7} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL8} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL9} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL10} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL11} alt="Random dog picture"></img>
+      <img className="dogPic" src={imageURL12} alt="Random dog picture"></img>
+    </div>
   );
 }
 
 export default GiphyImg;
 
-// We're now calling the api 20 times (which is wasteful) but we're only adding the picture to our array if it's a jpg
-
-// Maybe a better way to do it would be to establish the array, then while the length is less than n call the api...... We could also allow the user to pick the length to change difficulty
--- src/components/master.jsx
+++ src/components/master.jsx
@@ -15,3 +15,10 @@ function MainPage() {
 
 
 export default MainPage
+
+// Ok. We've successfully rendered 12 random images from API calls in the GiphyImg component. 
+// We need to reOrganize a bit. 
+// We need to get the image URL's then make a game tile with each one. 
+
+// We'll render all the game tiles on the first turn, then let the user click one. 
+// Then we'll reoeder the tiles and have the user click again. If they click a tile that's already been clicked -- They lose!
