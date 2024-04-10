b27a82fca1ab20f8dd5f06cb4f532e2998a49c65
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-10 11:04:42 -0700

    Pass urls to gameboard in array format

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -1,23 +1,10 @@
 import { useState, useEffect } from "react";
 
-function Gameboard({
-  imageURL1,
-  imageURL2,
-  umageURL3,
-  imageURL4,
-  imageURL5,
-  imageURL6,
-  imageURL7,
-  imageURL8,
-  imageURL9,
-  imageURL10,
-  imageURL11,
-  imageURL12,
-}) {
-  //  So we have our gameboard component. Buuuut we'll need to access our URL's if we wanna render our cards
+function Gameboard({urlList}) {
 
   return <div className="gameboard">
-    <img src={imageURL1} alt="Random dog pic" />
+    <img src={urlList[0]} alt="Random dog pic" />
+    <img src={urlList[1]} alt="Random dog pic" />
   </div>;
 }
 
--- src/components/master.jsx
+++ src/components/master.jsx
@@ -8,94 +8,41 @@ import Gameboard from "./gameboard";
 function MainPage() {
   const [currentScore, setCurrentScore] = useState(0);
   const [topScore, setTopScore] = useState(0);
-
-
-  const [imageURL1, setImageURL1] = useState("");
-  const [imageURL2, setImageURL2] = useState("");
-  const [imageURL3, setImageURL3] = useState("");
-  const [imageURL4, setImageURL4] = useState("");
-  const [imageURL5, setImageURL5] = useState("");
-  const [imageURL6, setImageURL6] = useState("");
-  const [imageURL7, setImageURL7] = useState("");
-  const [imageURL8, setImageURL8] = useState("");
-  const [imageURL9, setImageURL9] = useState("");
-  const [imageURL10, setImageURL10] = useState("");
-  const [imageURL11, setImageURL11] = useState("");
-  const [imageURL12, setImageURL12] = useState("");
-
-
+  const [urlList, setUrlList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      let urlList = [];
-
-      while (urlList.length < 13) {
-        console.log(`iteration`)
-        console.log(`urlList.length: ${urlList.length}`);
+      let newArray = [];
+      while (newArray.length < 12) {
         const result = await fetch("https://random.dog/woof.json");
         result.json().then((json) => {
           const toString = json.url.toString();
           let last3 = toString.substr(toString.length - 3);
           last3 = last3.toLowerCase();
           if (last3 == "jpg") {
-            urlList.push(json.url);
-            console.log(`Added a photo to the list`);
+            newArray.push(json.url);
+            console.log(`Added a suitable url (${newArray.length})`);
           }
         });
       }
-      setImageURL1(urlList[0]);
-      setImageURL2(urlList[1]);
-      setImageURL3(urlList[2]);
-      setImageURL4(urlList[3]);
-      setImageURL5(urlList[4]);
-      setImageURL6(urlList[5]);
-      setImageURL7(urlList[6]);
-      setImageURL8(urlList[7]);
-      setImageURL9(urlList[8]);
-      setImageURL10(urlList[9]);
-      setImageURL11(urlList[10]);
-      setImageURL12(urlList[11]);
+
+      console.log(`newArray is long enough.Length: ${newArray.length}`);
+      setUrlList(newArray);
     };
 
     fetchData();
   }, []);
 
-
-
-
-
-
   return (
     <div className="page">
       <h1>Memory Card Project</h1>
-      <Scoreboard currentScore={currentScore} topScore = {topScore}/>
-      <Gameboard
-      imageURL1 ={imageURL1}
-      imageURL2 ={imageURL2}
-      imageURL3 ={imageURL3}
-      imageURL4 ={imageURL4}
-      imageURL5 ={imageURL5}
-      imageURL6 ={imageURL6}
-      imageURL7 ={imageURL7}
-      imageURL8 ={imageURL8}
-      imageURL9 ={imageURL9}
-      imageURL10 ={imageURL10}
-      imageURL11 ={imageURL11}
-      imageURL12 ={imageURL12}
-      />
-      <Clock/>
+      <Scoreboard currentScore={currentScore} topScore={topScore} />
+      <Gameboard urlList={urlList} />
+      <Clock />
     </div>
   );
 }
 
+export default MainPage;
 
-export default MainPage
-
-// Ok. We've successfully rendered 12 random images from API calls in the GiphyImg component. 
-// We need to reOrganize a bit. 
-// We need to get the image URL's then make a game tile with each one. 
-
-// We'll render all the game tiles on the first turn, then let the user click one. 
-// Then we'll reoeder the tiles and have the user click again. If they click a tile that's already been clicked -- They lose!
-
-// Ideally we fetch all the urls we need in one place, then pass them to a prop ro render the cards?
+//  We're now calling the api 12 times in master, then passing our url objects to gameboard where we can make the cards. 
