9c8a531132796ea575d95f5023e1522262a2c974
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-10 12:31:12 -0700

    Render 12 game cards

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -2,9 +2,21 @@ import { useState, useEffect } from "react";
 
 function Gameboard({urlList}) {
 
+
+const gameCards = [];
+for (let i = 0;i < 12;i++){
+    let urlText = urlList[i];
+    let key = i;
+    gameCards.push(
+    <div className="gameCard" key = {key}>
+        <img src= {urlText} alt="random dog picture"></img>
+    </div>)
+}
+
+
+
   return <div className="gameboard">
-    <img src={urlList[0]} alt="Random dog pic" />
-    <img src={urlList[1]} alt="Random dog pic" />
+{gameCards}
   </div>;
 }
 
