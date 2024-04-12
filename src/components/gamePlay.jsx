dbb987d2d0ae4a916c984f36217f302a0f03469b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-11 17:48:37 -0700

    Call scramble function when card is clicked

--- src/components/gamePlay.jsx
+++ src/components/gamePlay.jsx
@@ -1,26 +1,33 @@
 import { useState, useEffect } from "react";
 
+function scramble(key) {
+  const cards = document.getElementsByClassName("gameCard");
+  console.log(`Scramble function called. Cards: ${cards}`);
+  console.log(`this.key: ${this.key}`);
+  console.log(this.getAttribute("key"));
+console.log(`clicked key: ${key}`);
+console.log(`key.target.parentElement: ${key.target.parentElement})}`)
+console.log(`key.target.parentElement.getAttribute("key"): ${key.target.parentElement.getAttribute("key")})}`)
 
-
-
-function scramble() {
-    const cards = document.getElementsByClassName("gameCard");
-    console.log(`Scramble function called. Cards: ${cards}`);
-    
+  // Now if we could just get this card's key??
+  // Remove event listeners?
 }
-
 
 function beginGame() {
-    const startupMessage = "Welcome to Memory Card! Click on a dog to cause the board to shuffle. You get a point if you haven't clicked that piece before. You lose if you repeat a click";
-    setTimeout(()=> {
-    alert(startupMessage);
-    }, 500)
+  const startupMessage =
+    "Welcome to Memory Card! Click on a dog to cause the board to shuffle. You get a point if you haven't clicked that piece before. You lose if you repeat a click";
+  setTimeout(() => {
+    // alert(startupMessage);
+    console.log(startupMessage);
+    const cards = document.getElementsByClassName("gameCard");
 
+    console.log(`cards: ${cards}`);
+    console.log(`cards[0]: ${cards[0]}`);
 
-
-
-
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", scramble);
+    }
+  }, 6000);
 }
 
-
-export default beginGame
+export default beginGame;
